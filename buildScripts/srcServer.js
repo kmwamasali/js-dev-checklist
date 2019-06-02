a8c552c12bdb9118e65a010c3c8bee0a2835dd70
Kevin <kmwamasali@example.com>
2019-06-02 13:09:02 -0700

    Add Babel for Transpilation

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import { join } from 'path';
+import open from 'open';
 
 var port = 3000;
 var app = express();
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(err){
