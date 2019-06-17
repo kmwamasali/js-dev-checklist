b572b3310d8ea399c28671df2e522ca53e3d5c16
Kevin <kmwamasali@example.com>
2019-06-17 05:00:14 -0700

    Dependecy fixes and updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import numeral from 'numeral';
 
 const amount = numeral(1000).format('$0,0.00');
-console.log(`A CMS starts from ${amount} by Castle-tech Labs`);
+
+console.log(`A CMS starts from ${amount} by Castle-tech Labs`); // eslint-disable-line no-console
